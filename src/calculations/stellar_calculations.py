ba2d68b5e532b79e3820561e12ff9fd5bb2c2c40
AƧTЯOƆAT <jinglinzhao@gmail.com>
2025-12-18 14:45:21 +0100

    Dec 18 backup

--- src/calculations/stellar_calculations.py
+++ src/calculations/stellar_calculations.py
@@ -112,7 +112,7 @@ SOLAR_T_EFF = 5780.0  # K - Solar effective temperature
 SOLAR_MASS = 1.0      # Solar masses
 SOLAR_LUMINOSITY = 1.0  # Solar luminosities
 SOLAR_GRANULATION_RMS = 0.333  # m/s - Solar granulation RMS (excluding supergranulation)
-# SOLAR_GRANULATION_RMS = 0.8  # m/s - Solar granulation RMS (excluding supergranulation)
+# SOLAR_GRANULATION_RMS = 0.8  # m/s - Solar granulation RMS (including supergranulation)
 
 def calculate_granulation_noise(T_eff, mass, luminosity):
     """
